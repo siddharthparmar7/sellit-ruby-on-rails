804b1c9089bf4cb8c8d6d013238f39ea0a645798
Sid Parmar <Sid@Sids-MBP.hitronhub.home>
2017-03-24 18:45:51 -0700

    kaminari is working for pagination

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page]).per(9)
   end
 
   # GET /items/1
